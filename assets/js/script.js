a0edd94d96c52ffaf827ca6dd9b6240d9b7c09ee
panecatl <ypanecatl24@gmail.com>
2022-05-29 09:29:39 -0500

    drag to erase on regular and phone screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -187,6 +187,75 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+// make the draggable happen
+$(".card .list-group").sortable({
+  // connectWith links these sortable lists with any other lists that have the same class
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  // creates a copy of the dragged element and move the copy
+  helper: "clone",
+  activate: function(event) {
+    // console.log("activate", this);
+  },
+  deactivate: function(event) {
+    // console.log("deactivate", this);
+  },
+  over: function(event) {
+    // console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+    
+    console.log(tempArr);
+  }
+});
+
+// drag down to trash
+$("#trash").droppable({
+  // class we are looking for
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    // the jquery ui that allows it to be dragged and dropped
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 
 // load tasks for the first time
 loadTasks();
